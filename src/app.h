b63e511081008b00890ad994d3ebb8fae97bb886
Johan S. Macias <johansmacias@outlook.com>
2021-10-08 10:49:24 -0500

    Add WisBlock-API library and remove files.

--- src/app.h
+++ src/app.h
@@ -16,6 +16,27 @@
 #include <Arduino.h>
 /** Add you required includes after Arduino.h */
 
+/** Include the WisBlock-API */
+#include <WisBlock-API.h> // Click to install library: http://librarymanager/All#WisBlock-API
+
+// Debug output set to 0 to disable app debug output
+#ifndef MY_DEBUG
+#define MY_DEBUG 1
+#endif
+
+#if MY_DEBUG > 0
+#define MYLOG(tag, ...)           \
+  do                            \
+  {                             \
+    if (tag)                  \
+      PRINTF("[%s] ", tag); \
+    PRINTF(__VA_ARGS__);      \
+    PRINTF("\n");             \
+  } while (0)
+#else
+#define MYLOG(...)
+#endif
+
 /** Application function definitions */
 void setup_app(void);
 bool init_app(void);
